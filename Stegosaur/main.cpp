f5bb8ad2f403b53e7b7f46825cf8d3e988ef9afb
Gudbrand <gudbrandduff@gmail.com>
2016-04-28 08:30:04 +0800

    Initial Commit

---
+++ Stegosaur/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Stegosaur
+//
+//  Created by Gudbrand Tandberg on 28/04/16.
+//  Copyright (c) 2016 Duff Development. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
